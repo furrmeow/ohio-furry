9763b6f6264d6e4802b9be0a47c5d74b3d9bde50
FurUwU <127991427+FurUwU@users.noreply.github.com>
2023-03-31 11:48:14 +0300

    project

---
+++ index.js
@@ -0,0 +1,30 @@
+// javascript
+const sText = document.querySelector(".name")
+const Button1 = document.querySelector(".magenta")
+const Button2 = document.querySelector(".gray")
+
+// functions
+function Change() {
+    sText.style.color = "blue";
+}
+
+function Enter() {
+    Button1.style.color = "white";
+}
+
+function Leave() {
+    Button1.style.color = "black";
+}
+
+
+function Change2() {
+    sText.style.color = "black";
+}
+
+function Enter2() {
+    Button2.style.color = "white";
+}
+
+function Leave2() {
+    Button2.style.color = "black";
+}
